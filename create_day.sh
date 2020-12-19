4f30da5d18acb903c5a1fe0e016f1d8abfc83425
Jamesbarford <jamesbarfordevans@gmail.com>
2020-12-19 23:02:37 +0000

    bash creates boilerplate

--- create_day.sh
+++ create_day.sh
@@ -8,15 +8,31 @@ PART=part
 
 mkdir ./$FOLDER
 
-for part in {1..2}
-do
+for part in {1..2}; do
     DAY_PART=$PART$part
 
     mkdir ./$FOLDER/$DAY_PART ./$FOLDER/$DAY_PART/$OUTPUT
-    touch ./$FOLDER/$DAY_PART/solve.cpp
+
+    cat >./$FOLDER/$DAY_PART/solve.cpp \
+        <<-EOCPP
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(void)
+{
+    std::ifstream infile("../input.txt");
+    std::string line;
+
+    while (std::getline(infile, line))
+    {
+    }
+}
+EOCPP
+
     cp ./makefile_template ./$FOLDER/$DAY_PART/makefile
 done
 
-curl --cookie $TOKEN  https://adventofcode.com/2020/day/$1/input -o ./$FOLDER/input.txt
+curl --cookie $TOKEN https://adventofcode.com/2020/day/$1/input -o ./$FOLDER/input.txt
 
 echo "Happy coding!!! 🎅🏻🎄❄️"
